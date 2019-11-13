6cc829c371e4100bddf254d3a737860f714902f7
Anderson Chen <ander.chen00@gmail.com>
2019-11-13 18:55:35 -0300

    Livecode advanced routing batch-327

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :gardens, only: [:index, :show] do
+    resources :plants, only: :create
+  end
+  resources :plants, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+
+Garden.destroy_all
+
+puts "Database clean!"
+
+puts "Creating Gardens...."
+
+garden_attributes  = [
+  {
+    name: "Jardim Eden",
+    banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg"
+  },
+  {
+    name: "Davis Picnic Garden",
+    banner_url:"https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
+  }
+]
+
+Garden.create!(garden_attributes)
+
+puts "#{Garden.count} gardens created!"
+
